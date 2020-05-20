802b8ef2dde5ba3fb7fe9891004737afff30474a
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-05-20 22:19:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,16 +7,18 @@ get '/reversename/:name' do
   "#{@reversename}"
 end
 get '/square/:number' do 
-  @sqare = params[:number]** 2 
+  @sqare = params[:number].to_i ** 2 
   "#{@square}"
 end
 get 'say/:number/:phrase' do 
-  @number = params[:number]
+  @number = params[:number].to_i
   @phrase = params[:phrase]
   @number.times {"#{phrase}"}
 end 
 get '/say/:word1/:word2/:word3/:word4/:word5' do 
   "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}"
 end 
-get '/:operation/:number1/:number2'
+get '/:operation/:number1/:number2' do 
+  
+end 
 end
