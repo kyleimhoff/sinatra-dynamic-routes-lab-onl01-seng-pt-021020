0bc4d94f26ed68c34c8a42fdbd4c3051a7420ad2
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-05-20 22:28:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -28,13 +28,13 @@ get '/:operation/:number1/:number2' do
   
   case params[:operation]
     when 'add'
-      answer = (number1 + number2).to_s
+      answer = (num1 + num2).to_s
     when 'subtract'
-      answer = (number1 - number2).to_s
+      answer = (num1 - num2).to_s
     when 'multiply'
-      answer = (number1 * number2).to_s
+      answer = (num1 * num2).to_s
     when 'divide'
-      answer = (number1 / number2).to_s
+      answer = (num1 / num2).to_s
     end
   
 end 
