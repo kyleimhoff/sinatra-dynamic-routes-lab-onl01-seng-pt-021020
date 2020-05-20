fea171c004a3ddf9ef3934292f83ab2e660bc9d6
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-05-20 22:16:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ get 'say/:number/:phrase' do
   @number.times {"#{phrase}"}
 end 
 get '/say/:word1/:word2/:word3/:word4/:word5' do 
-  "#{params[:word1]} "
+  "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}"
 end 
 
 end
